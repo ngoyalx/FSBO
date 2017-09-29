b90490ced0150bc2dd0aee32366871c32024dccf
ngoyalx <ngoyal@xebia.com>
2017-09-29 16:21:03 +0530

    updated the code for saucelabs execution

--- src/main/java/DriverFactory.java
+++ src/main/java/DriverFactory.java
@@ -20,7 +20,7 @@ public class DriverFactory {
     public static URL url;
     public File androidNativeappPath;
     public File IOSNativeappPath;
-    public String isRemoteExecution;
+    public String executionType;
 
 
     public DriverFactory(){
@@ -33,29 +33,43 @@ public class DriverFactory {
 
     public AppiumDriver getDriver() throws MalformedURLException {
         setAppPath();
-        if(GlobalProperties.getPropertyMap().get("AUT").equalsIgnoreCase("androidnative")){
-            driver = new AndroidDriver(url,getAndroidNativeCapabilities(capabilities));
-        }else if(GlobalProperties.getPropertyMap().get("AUT").equalsIgnoreCase("androidweb")){
-            driver = new AndroidDriver(url,getAndroidWebCapabilities(capabilities));
-        }else if(GlobalProperties.getPropertyMap().get("AUT").equalsIgnoreCase("iosnative")){
-            driver = new IOSDriver(url,getIOSNativeCapabilities(capabilities));
-        }else if(GlobalProperties.getPropertyMap().get("AUT").equalsIgnoreCase("iosweb")){
-            driver = new IOSDriver(url,getIOSWebCapabilities(capabilities));
+        if(this.executionType.equalsIgnoreCase("saucelabs")){
+            if(GlobalProperties.getPropertyMap().get("AUT").equalsIgnoreCase("androidnative")){
+                driver = new AndroidDriver(url,getAndroidNativeSauceLabCapabilities(capabilities));
+            }else if(GlobalProperties.getPropertyMap().get("AUT").equalsIgnoreCase("androidweb")){
+                driver = new AndroidDriver(url,getAndroidWebSauceLabCapabilities(capabilities));
+            }else if(GlobalProperties.getPropertyMap().get("AUT").equalsIgnoreCase("iosnative")){
+                driver = new IOSDriver(url,getIOSNativeSauceLabCapabilities(capabilities));
+            }else if(GlobalProperties.getPropertyMap().get("AUT").equalsIgnoreCase("iosweb")){
+                driver = new IOSDriver(url,getIOSWebSauceLabCapabilities(capabilities));
+            }
+        }else{
+            if(GlobalProperties.getPropertyMap().get("AUT").equalsIgnoreCase("androidnative")){
+                driver = new AndroidDriver(url,getAndroidNativeCapabilities(capabilities));
+            }else if(GlobalProperties.getPropertyMap().get("AUT").equalsIgnoreCase("androidweb")){
+                driver = new AndroidDriver(url,getAndroidWebCapabilities(capabilities));
+            }else if(GlobalProperties.getPropertyMap().get("AUT").equalsIgnoreCase("iosnative")){
+                driver = new IOSDriver(url,getIOSNativeCapabilities(capabilities));
+            }else if(GlobalProperties.getPropertyMap().get("AUT").equalsIgnoreCase("iosweb")){
+                driver = new IOSDriver(url,getIOSWebCapabilities(capabilities));
+            }
         }
         return driver;
     }
 
 
     private void setAppPath() throws MalformedURLException {
-        this.isRemoteExecution = GlobalProperties.getPropertyMap().get("isRemoteExecution");
-        if(this.isRemoteExecution.equalsIgnoreCase("true")){
+        this.executionType = GlobalProperties.getPropertyMap().get("executionType");
+        if(this.executionType.equalsIgnoreCase("remote")){
             androidNativeappPath = new File(GlobalProperties.remoteAppDir + File.separator + GlobalProperties.getPropertyMap().get("androidAppName"));
             IOSNativeappPath = new File(GlobalProperties.remoteAppDir + File.separator + GlobalProperties.getPropertyMap().get("IOSAppName"));
             url = new URL(GlobalProperties.getPropertyMap().get("remoteUrl") + "/wd/hub");
-        }else{
+        }else if(this.executionType.equalsIgnoreCase("local")){
             androidNativeappPath = new File(GlobalProperties.localAppDir + File.separator + GlobalProperties.getPropertyMap().get("androidAppName"));
             IOSNativeappPath = new File(GlobalProperties.localAppDir + File.separator + GlobalProperties.getPropertyMap().get("IOSAppName"));
             url = new URL(GlobalProperties.getPropertyMap().get("localUrl") + "/wd/hub");
+        }else if(this.executionType.equalsIgnoreCase("saucelabs")){
+            url = new URL(GlobalProperties.getPropertyMap().get("sauceLabUrl") + "/wd/hub");
         }
     }
 
@@ -72,7 +86,6 @@ public class DriverFactory {
         capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
         capabilities.setCapability(MobileCapabilityType.APP,androidNativeappPath.getAbsolutePath());
         capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Emulator");
-        //capabilities.setCapability(MobileCapabilityType.ORIENTATION,"landscape");
         return capabilities;
     }
 
@@ -89,7 +102,6 @@ public class DriverFactory {
         capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "IOS");
         capabilities.setCapability(MobileCapabilityType.APP, IOSNativeappPath.getAbsolutePath());
         capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, GlobalProperties.getPropertyMap().get("iosDeviceName"));
-        //capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,"XCUITest");
         return capabilities;
     }
 
@@ -97,6 +109,38 @@ public class DriverFactory {
         capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, GlobalProperties.getPropertyMap().get("iosPlatformVersion"));
         capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "IOS");
         capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, GlobalProperties.getPropertyMap().get("iosDeviceName"));
+        capabilities.setCapability(MobileCapabilityType.BROWSER_NAME,"Safari");
+        return capabilities;
+    }
+
+    private DesiredCapabilities getAndroidNativeSauceLabCapabilities(DesiredCapabilities capabilities){
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, GlobalProperties.getPropertyMap().get("androidPlatformVersion"));
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        capabilities.setCapability(MobileCapabilityType.APP,"sauce-storage:ApiDemos-debug.apk");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Emulator");
+        return capabilities;
+    }
+
+    private DesiredCapabilities getIOSNativeSauceLabCapabilities(DesiredCapabilities capabilities){
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, GlobalProperties.getPropertyMap().get("iosPlatformVersion"));
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "ios");
+        capabilities.setCapability(MobileCapabilityType.APP,"sauce-storage:UICatalogSauce.zip");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 6 Simulator");
+        return capabilities;
+    }
+
+    private DesiredCapabilities getAndroidWebSauceLabCapabilities(DesiredCapabilities capabilities){
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, GlobalProperties.getPropertyMap().get("androidPlatformVersion"));
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Emulator");
+        capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "Chrome");
+        return capabilities;
+    }
+
+    private DesiredCapabilities getIOSWebSauceLabCapabilities(DesiredCapabilities capabilities){
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, GlobalProperties.getPropertyMap().get("iosPlatformVersion"));
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "ios");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"iPhone 6 Simulator");
         capabilities.setCapability(MobileCapabilityType.BROWSER_NAME,"Safari");
         return capabilities;
     }
